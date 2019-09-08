d3fcaf4593dbf72c1e480e7665e6e821c4d1a9c0
R-U-ON <yuvalperlov@gmail.com>
2019-09-08 20:32:09 +0300

    tests

---
+++ R-U-ONTests/R_U_ONTests.m
@@ -0,0 +1,34 @@
+//
+//  R_U_ONTests.m
+//  R-U-ONTests
+//
+//  Created by YUVAL PERLOV on 1/1/14.
+//  Copyright (c) 2014 ruon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface R_U_ONTests : XCTestCase
+
+@end
+
+@implementation R_U_ONTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
